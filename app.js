48ae2257c0278d3efaba85e5b40379a2b5b22c1f
DamianRivas <drivas1993@gmail.com>
2017-12-06 11:59:41 -0500

    Add working delete functionality

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function onReady() {
 
   // ADD TO THE TO-DO LIST
   function createNewToDo() {
-    if(!newToDoText.value) { return; }
+    if (!newToDoText.value) { return; }
 
     toDos.push({
       title: newToDoText.value,
@@ -20,26 +20,47 @@ function onReady() {
 
   // DELETE FROM THE TO-DO LIST
   function deleteToDos() {
+    let listOfLists = document.getElementsByTagName('li');
+
+    for (let i = 0; i < listOfLists.length; i++) {
+      if (listOfLists[i].querySelector("input").checked) {
+        toDos = toDos.filter(todo => !todo.complete);
+      }
+    }
   }
 
 
   // RENDER THE TO-DO LIST
   function renderTheUI() {
 
+    id = 0;
     toDoList.textContent = '';
 
     toDos.forEach(toDo => {
-       const newToDo = document.createElement('li');
-       const checkbox = document.createElement('input');
-       checkbox.type = 'checkbox';
-       const title = document.createElement('span');
-       title.textContent = toDo.title
+      const newToDo = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      const title = document.createElement('span');
+      title.textContent = toDo.title
 
-       toDoList.appendChild(newToDo);
-       newToDo.appendChild(checkbox);
-       newToDo.appendChild(title);
+      toDoList.appendChild(newToDo);
+      newToDo.appendChild(checkbox);
+      newToDo.appendChild(title);
 
+      if (toDo.complete === true) {
+        checkbox.checked = true;
+      }
+
+      checkbox.addEventListener('change', event => {
+        if (checkbox.checked) {
+          toDo.complete = true;
+        } else {
+          toDo.complete = false;
+        }
+      });
+      toDo.id = id++
     });
+
   }
 
   // CREATE A NEW TO-TO ON SUBMIT
@@ -50,6 +71,7 @@ function onReady() {
     newToDoText.value = '';
   });
 
+  // DELETE COMPLETED TODOS
   deleteToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     deleteToDos();
@@ -60,6 +82,6 @@ function onReady() {
 
 }
 
-window.onload = function() {
+window.onload = function () {
   onReady();
 };
